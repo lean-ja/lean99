f99be1b98f54324304f67a466d99aa11b3d31038
Seasawher <tellagoodstory@icloud.com>
2024-03-15 08:46:39 +0900

    setup `mdgen` and `import-all`

--- lakefile.lean
+++ lakefile.lean
@@ -8,5 +8,28 @@ package «Src» where
     ⟨`relaxedAutoImplicit, false⟩
   ]
 
+@[default_target]
 lean_lib «Src» where
   -- add library configuration options here
+
+require «import-all» from git
+  "https://github.com/Seasawher/import-all" @ "main"
+
+require mdgen from git
+  "https://github.com/Seasawher/mdgen" @ "main"
+
+def runCmd (cmd : String) (args : Array String) : ScriptM Bool := do
+  let out ← IO.Process.output {
+    cmd := cmd
+    args := args
+  }
+  let hasError := out.exitCode != 0
+  if hasError then
+    IO.eprint out.stderr
+  return hasError
+
+script build do
+  if ← runCmd "lake exe mdgen" #["Src", "md"] then return 1
+  if ← runCmd "lake exe import_all" #["Src"] then return 1
+  if ← runCmd "mdbook build" #[] then return 1
+  return 0
