596a53eb7662b416d28a1a2537958950e4a01b78
Seasawher <tellagoodstory@icloud.com>
2024-03-15 08:52:43 +0900

    add syntax highlight

--- Src/README.lean
+++ Src/README.lean
@@ -1,3 +1,13 @@
 /- # Lean Book
 Lean Book is the mdbook template configured for the Lean Project.
 -/
+
+/-! ## Syntax Highlighting
+Here is an example of Lean code block.-/
+
+variable {P Q : Prop}
+
+example : P → (P ∨ Q) := by
+  intro hP
+  apply Or.inl
+  exact hP
