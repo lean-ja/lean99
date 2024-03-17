1c9cb94a5ddb0bd9ad2c5528b2004c5c0ad805e2
Seasawher <tellagoodstory@icloud.com>
2024-03-17 18:41:50 +0900

    format code style

--- Src/Problem3.lean
+++ Src/Problem3.lean
@@ -5,22 +5,24 @@ Find the K'th element of a list.
 
 variable {α : Type}
 
-def element_at (l : List α)(k:Nat) : Option α :=
+def elementAt (l : List α) (k : Nat) : Option α :=
   -- sorry
-  match k,l with
-  | _,[] => none
-  | 0,_ => none
-  | 1,[a] => some a
-  | 1,a :: _ => some a
-  | Nat.succ k,_ :: a => element_at a k
+  match k, l with
+  | _, [] => none
+  | 0, _ => none
+  | 1, [a] => some a
+  | 1, a :: _ => some a
+  | Nat.succ k, _ :: a => elementAt a k
   -- sorry
+
 -- The following is a test case, you don't need to edit it.
-example : element_at ['a','b','c','d','e'] 3 = some 'c' := by rfl
 
-example : element_at ['a','b','c','d','e'] 6 = none := by rfl
+example : elementAt ['a', 'b', 'c', 'd', 'e'] 3 = some 'c' := by rfl
 
-example : element_at ['a','b','c','d','e'] 0 = none := by rfl
+example : elementAt ['a', 'b', 'c', 'd', 'e'] 6 = none := by rfl
 
-example : element_at ([] : List α) 0 = none := by rfl
+example : elementAt ['a', 'b', 'c', 'd', 'e'] 0 = none := by rfl
 
-example : element_at [1,2,3] 2 = some 2 := by rfl
+example : elementAt ([] : List α) 0 = none := by rfl
+
+example : elementAt [1, 2, 3] 2 = some 2 := by rfl
--- Src/Problem4.lean
+++ Src/Problem4.lean
@@ -12,11 +12,13 @@ def myLength (l : List α) : Nat :=
   | [_] => 1
   | _ :: a => (myLength a).succ
   -- sorry
+
 -- The following is a test case, you don't need to edit it.
-example : myLength [123,456,789] = 3 := by rfl
 
-example : myLength ['L','e','a','n','4'] = 5 := by rfl
+example : myLength [123, 456, 789] = 3 := by rfl
 
-example : myLength [False,True,True] = 3 := by rfl
+example : myLength ['L', 'e', 'a', 'n', '4'] = 5 := by rfl
+
+example : myLength [False, True, True] = 3 := by rfl
 
 example : myLength ([] : List α) = 0 := by rfl
