9e58cde30101739df392b908410ae84e38d23669
csharpython <121863991+csharpython@users.noreply.github.com>
2024-03-17 14:03:03 +0900

    Create Probrem4.lean

---
+++ Src/Probrem4.lean
@@ -0,0 +1,22 @@
+/-
+# Problem 4
+Find the K'th element of a list.
+-/
+
+variable {α : Type}
+
+def myLength (l : List α) : Nat :=
+  --sorry
+  match l with
+  | [] => 0
+  | [_] => 1
+  | _ :: a => (myLength a).succ
+  --sorry
+-- The following is a test case, you don't need to edit it.
+example : myLength [123,456,789] = 3 := by rfl
+
+example : myLength ['L','e','a','n','4'] = 5 := by rfl
+
+example : myLength [False,True,True] = 3 := by rfl
+
+example : myLength ([] : List α) = 0 := by rfl
