3f28c9bbf158440168e2d7e88a69acba924a5be6
csharpython <121863991+csharpython@users.noreply.github.com>
2024-03-17 18:52:29 +0900

    Update Problem3.lean
    不要な場合分けの修正

--- Src/Problem3.lean
+++ Src/Problem3.lean
@@ -7,12 +7,11 @@ variable {α : Type}
 
 def elementAt (l : List α) (k : Nat) : Option α :=
   -- sorry
-  match k, l with
-  | _, [] => none
-  | 0, _ => none
-  | 1, [a] => some a
-  | 1, a :: _ => some a
-  | Nat.succ k, _ :: a => elementAt a k
+  match l, k with
+  | [] , _  => none
+  | _ , 0 => none
+  | a :: _ , 1 => some a
+  | _ :: a , Nat.succ k => elementAt a k
   -- sorry
 
 -- The following is a test case, you don't need to edit it.
