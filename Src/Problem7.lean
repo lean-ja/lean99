0e54c440424d2c1c87faa168cfa8a62dedeb8cb6
Seasawher <tellagoodstory@icloud.com>
2024-03-18 09:43:18 +0900

    solve Problem7 🎉

--- Src/Problem7.lean
+++ Src/Problem7.lean
@@ -11,27 +11,52 @@ inductive NestedList (α : Type) where
 | elem : α → NestedList α
 | list : List (NestedList α) → NestedList α
 
-open Std
+section
+  /-!
+  ## Pretty printing of NestedList
+  Display `NestedList` in a readable manner when you run `#eval`.
+  When solving this problem, do not mind the contents of this section.
 
-protected def NestedList.repr [Repr α] (a : NestedList α) (n : Nat) : Format :=
-  let _ : ToFormat α := ⟨repr⟩
-  match a, n with
-  | elem x, _ => reprPrec x n
-  | list as, _ =>
-    -- this code raise an error! :(
-    Format.bracket "[" (Format.joinSep as ("," ++ Format.line)) "]"
+  The following code was provided by Mario Carneiro.
+  -/
+  open Std
 
-instance [Repr α] : Repr (NestedList α) where
-  reprPrec := NestedList.repr
+  partial def NestedList.repr [Repr α] (a : NestedList α) (n : Nat) : Format :=
+    let _ : ToFormat (NestedList α) := ⟨(NestedList.repr · 0)⟩
+    match a, n with
+    | elem x, _ => reprPrec x n
+    | list as, _ => Format.bracket "[" (Format.joinSep as ("," ++ Format.line)) "]"
+
+  instance [Repr α] : Repr (NestedList α) where
+    reprPrec := NestedList.repr
+end
+
+-- Here is an example of how to use the `NestedList` type.
 
 open NestedList
 
 def sample : NestedList Nat :=
   list [elem 1, list [elem 2, elem 3], elem 4]
 
-/-
-What I want is:
-
-[1, [2, 3], 4]
--/
 #eval sample
+
+def empty : NestedList String := list []
+
+#eval empty
+
+/-- flatten the list structure -/
+def flatten (nl : NestedList α) : List α :=
+  -- sorry
+  match nl with
+  | elem x => [x]
+  | list [] => []
+  | list (x :: xs) => flatten x ++ flatten (list xs)
+  -- sorry
+
+-- The following code is a test case and you should not change it.
+
+example : flatten (elem 5) = [5] := rfl
+
+example : flatten sample = [1, 2, 3, 4] := rfl
+
+example : flatten (empty) = [] := rfl
