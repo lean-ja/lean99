aace80c0a913ebd1ab864ea66f4ae12c5405885c
Seasawher <tellagoodstory@icloud.com>
2024-03-19 10:15:03 +0900

    format code

--- Src/Problem20.lean
+++ Src/Problem20.lean
@@ -6,19 +6,20 @@ variable {α : Type}
 
 def removeAt (l : List α) (n : Nat) : List α :=
   -- sorry
-  match l,n with
-  | [] , _ => []
-  | _ , 0 => l
-  | x :: b , m + 2 => x :: removeAt b (m+1)
-  | _ :: b , _ => b
+  match l, n with
+  | [], _ => []
+  | _, 0 => l
+  | _ :: b, 1 => b
+  | x :: b, m + 2 => x :: removeAt b (m + 1)
   -- sorry
 
 -- The following code is a test case and you should not change it.
-example : removeAt ['a','b','c','d'] 2 = ['a','c','d'] := rfl
 
-example : removeAt ['a','b','c','d'] 5 = ['a','b','c','d'] := rfl
+example : removeAt ['a', 'b', 'c', 'd'] 2 = ['a', 'c', 'd'] := rfl
 
-example : removeAt ['a','b','c','d'] 0 = ['a','b','c','d'] := rfl
+example : removeAt ['a', 'b', 'c', 'd'] 5 = ['a', 'b', 'c', 'd'] := rfl
+
+example : removeAt ['a', 'b', 'c', 'd'] 0 = ['a', 'b', 'c', 'd'] := rfl
 
 example : removeAt ['a'] 1 = [] := rfl
 
