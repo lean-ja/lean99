0df898c8d48de6437afe797206b37e8ec67a11dd
Seasawher <tellagoodstory@icloud.com>
2024-03-20 06:49:04 +0900

    update top page

--- Src/README.lean
+++ Src/README.lean
@@ -1,13 +1,15 @@
 /-
 # Lean 99: Ninety-Nine Lean Probelms
 
+Lean is a theorem prover, but it is also a programming language. Moreover, it is the best functional language ever! Let's learn functional programming with Lean. 😎
+
 These are Lean translations of [Ninety-Nine Haskell Problems](https://wiki.haskell.org/H-99:_Ninety-Nine_Haskell_Problems), which are themselves translations of [Ninety-Nine Lisp Probelms](https://www.ic.unicamp.br/~meidanis/courses/mc336/problemas-lisp/L-99_Ninety-Nine_Lisp_Problems.html).
 
 ## How to Play
 
-Every Lean code block in this book has a button to jump to the Lean Web Playground. Replace the `sorry` with your own code so that the test code passes.
+* Every Lean code block in this book has a button to jump to the Lean web playground. Replace the `sorry` with your own code so that the test code passes.
 
-You can see an answer by clicking on the <i class="fa fa-play"></i> button in the top right-hand corner of each page.
+* You can see an answer by clicking on the <i class="fa fa-play"></i> button in the top right-hand corner of each page.
 
 Good luck! 👍
 -/
