aac9b4e695003c55278cf634c069aa5db9dc5398
Seasawher <tellagoodstory@icloud.com>
2024-04-06 17:48:49 +0900

    refactor of code example

--- Src/Problem32.lean
+++ Src/Problem32.lean
@@ -3,6 +3,9 @@
 (Intermediate 🌟🌟) Determine the greatest common divisor of two positive integer numbers. Use the Euclid's algorithm.
 -/
 
+-- don't use this
+#check Nat.gcd
+
 /-- Euclidean algorithm -/
 def gcd (m n : Nat) : Nat :=
   -- sorry
@@ -18,12 +21,12 @@ def gcd (m n : Nat) : Nat :=
 
 -- The following codes are for test and you should not edit these.
 
-example : gcd 6 0 = 6 := by first | native_decide | rfl
+example : gcd 6 0 = 6 := by native_decide
 
-example : gcd 1 37 = 1 := by first | native_decide | rfl
+example : gcd 1 37 = 1 := by native_decide
 
-example : gcd 6 15 = 3 := by first | native_decide | rfl
+example : gcd 6 15 = 3 := by native_decide
 
-example : gcd 21 3 = 3 := by first | native_decide | rfl
+example : gcd 21 3 = 3 := by native_decide
 
-example : gcd 42998431 120019 = 1 := by first | native_decide | rfl
+example : gcd 42998431 120019 = 1 := by native_decide
