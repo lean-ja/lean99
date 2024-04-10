12ca2f0db29d65ee8ab466c7c3e26912e327e378
Seasawher <tellagoodstory@icloud.com>
2024-04-10 20:54:26 +0900

    fix code of P35

--- Src/Problem35.lean
+++ Src/Problem35.lean
@@ -9,9 +9,7 @@ def primeFactors (n : Nat) : List Nat :=
   -- sorry
   loop n 2 [] |>.reverse
   where loop (tgt candidate : Nat) (acc : List Nat) : List Nat :=
-    if candidate <= 1 || candidate > tgt then
-      panic! "candidate is out of range"
-    else if tgt ≤ 1 then
+    if tgt ≤ 1 || candidate > tgt then
       acc
     else if tgt % candidate = 0 then
       loop (tgt / candidate) candidate <| candidate :: acc
