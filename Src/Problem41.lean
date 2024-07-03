22a4b4c041b9cc60670f48ae34199755ef3eaa7d
Seasawher <tellagoodstory@icloud.com>
2024-07-03 13:56:33 +0900

    modify P40 and P41

--- Src/Problem41.lean
+++ Src/Problem41.lean
@@ -3,18 +3,18 @@
 (Intermediate 🌟🌟) A list of even numbers and their Goldbach compositions in a given range.
 -/
 
-def Nat.isPrime (n : Nat) : Bool :=
-  if n == 0 || n == 1 then
-    false
-  else
-    let properDivisors := List.range n
-      |>.drop 2
-      |>.filter (n % · == 0)
-    properDivisors.length == 0
+def Nat.isPrime (n : Nat) : Bool := Id.run do
+  if n ≤ 2 then
+    return false
+  for d in [2:n] do
+    if n % d = 0 then
+      return false
+    if d ^ 2 > n then
+      break
+  return true
 
 def goldbach (n : Nat) : Nat × Nat := Id.run do
-  -- sorry
-  for cand in (List.range n) do
+  for cand in [2:n] do
     if not cand.isPrime then
       continue
     let rest := n - cand
@@ -24,14 +24,19 @@ def goldbach (n : Nat) : Nat × Nat := Id.run do
 
   panic! "we've found a counter example of goldbach conjecture!! 🎉"
 
-
 def goldbachList (lower upper : Nat) (primeLower : Nat := 2) : List (Nat × Nat) :=
+  -- sorry
   List.range (upper + 1)
     |>.filter (· ≥ lower)
     |>.filter (· % 2 == 0)
     |>.map (fun n => (goldbach n))
-    |>.filter ( fun t => t.fst > primeLower)
+    |>.filter (fun t => t.fst > primeLower)
+  -- sorry
+
+-- The following codes are for test and you should not edit these.
 
 #guard goldbachList 9 20 == [(3, 7), (5, 7), (3, 11), (3, 13), (5, 13), (3, 17)]
+
+#guard goldbachList 9 20 3 == [(5, 7), (5, 13)]
 
 #guard goldbachList 4 2000 50 == [(73,919),(61,1321),(67,1789),(61,1867)]
