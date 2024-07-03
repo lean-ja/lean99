612ad00777d8175dc2ff063675d12b7dc9d26809
Seasawher <tellagoodstory@icloud.com>
2024-07-03 14:00:55 +0900

    modify P46

--- Src/Problem46.lean
+++ Src/Problem46.lean
@@ -4,12 +4,16 @@
 
 
 def table (p : Bool → Bool → Bool) : List (List Bool) :=
+  -- sorry
   [
     [true, true, p true true],
     [true, false, p true false],
     [false, true, p false true],
     [false, false, p false false]
   ]
+  -- sorry
+
+-- The following codes are for test and you should not edit these.
 
 #guard table (fun a b => And a (Or a b)) ==
   [
