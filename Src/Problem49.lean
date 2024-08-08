b69863b77899cfd6240b4c7f4a1cf32442f56193
Seasawher <tellagoodstory@icloud.com>
2024-08-08 21:28:13 +0900

    small fix in P49

--- Src/Problem49.lean
+++ Src/Problem49.lean
@@ -6,7 +6,7 @@ An n-bit Gray code is a sequence of n-bit strings constructed according to certa
 ```
 n = 1: C(1) = ['0','1'].
 n = 2: C(2) = ['00','01','11','10'].
-n = 3: C(3) = ['000','001','011','010',´110´,´111´,´101´,´100´].
+n = 3: C(3) = ['000','001','011','010','110','111','101','100'].
 ```
 
 Find out the construction rules and write a predicate with the following specification:
